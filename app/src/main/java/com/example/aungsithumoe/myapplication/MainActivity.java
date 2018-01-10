539c87a6067ac8d64155c9f192c8cc7373918105
aungsithumoe <aungsithumoe1@gmail.com>
2018-01-10 17:04:56 +0630

    aa

--- app/src/main/java/com/example/aungsithumoe/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aungsithumoe/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.aungsithumoe.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 int[] img={R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher};
@@ -15,5 +18,11 @@ int[] img={R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher};
         list=(ListView)findViewById(R.id.list);
         CustomAdapter adapter=new CustomAdapter(this,name,img);
         list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
